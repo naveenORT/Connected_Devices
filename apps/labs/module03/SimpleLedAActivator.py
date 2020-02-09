7a9a4c91354bfc0f10228493df141b7cc754cc01
naveenORT <rajendran.n@husky.neu.edu>
2020-02-08 23:07:02 -0500

    Assignment 3

--- apps/labs/module03/SimpleLedAActivator.py
+++ apps/labs/module03/SimpleLedAActivator.py
@@ -8,7 +8,7 @@ import threading
 from sense_hat import SenseHat
 import logging
 import time
-led_glow = SenseHat()
+
 class SimpleLedActivator():
     rate_in_sec = ''
     on_led = ''
@@ -17,14 +17,14 @@ class SimpleLedActivator():
     def __init__(self, time):
         self.rate_in_sec = time
         self.on_led = True
-    
+        self.led_glow = SenseHat()
     def setRED(self):
         
             r = [255,0,0]
             w = [255,255,255]
-            led_glow.show_letter("R")    
+            self.led_glow.show_letter("R")    
             time.sleep(1.5) 
-            '''       
+                   
             r = (255, 0, 0)  # rreen
             b = (0, 0, 0)  # Black    
             # Set up where each colour will display
@@ -40,16 +40,16 @@ class SimpleLedActivator():
             ]
             
             # Display these colours on the LED matrix
-            self.led_glow.set_pixels(creeper_pixels) '''        
+            self.led_glow.set_pixels(creeper_pixels)         
             return
         
     def setBLUE(self):    
             
             b = [0,0,255]
             w = [255,255,255]
-            led_glow.show_message("L")
+            self.led_glow.show_message("L")
             time.sleep(1.5)
-            '''
+            
             bl = (0, 0, 255)  # rreen
             b = (0, 0, 0)  # Black
             
@@ -66,6 +66,6 @@ class SimpleLedActivator():
             ]
             
             # Display these colours on the LED matrix
-            self.led_glow.set_pixels(creeper_pixels) '''                                          
+            self.led_glow.set_pixels(creeper_pixels)                                           
             return
         
