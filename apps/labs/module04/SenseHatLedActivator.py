ce80fa732e0e304d235c3eedc01f478cf828cc40
naveenORT <naveenORT@bitbucket.otg/naveenORT/iot-device.git>
2020-02-16 19:06:58 -0500

    Assignemnet 4

--- apps/labs/module04/SenseHatLedActivator.py
+++ apps/labs/module04/SenseHatLedActivator.py
@@ -37,8 +37,9 @@ class SimpleLedActivator():
     def show_i2c_LED(self, i2c_humidity_value):
         text_colour = [0, 255, 0]
         back_colour = [0, 0, 0]
-        x = str(i2c_humidity_value)
-        self.led_glow.show_message(x, 0.05, text_colour, back_colour)
+        x = float(i2c_humidity_value)
+        
+        self.led_glow.show_message(str(x), 0.05, text_colour, back_colour)
     '''
     Public function to make Sense Hat LED'S glow BLUE colour
     '''    
@@ -46,8 +47,8 @@ class SimpleLedActivator():
     def show_api_LED(self, api_humidity_value):    
         text_colour = [0,0,255]
         back_colour = [0,0,0]
-        y = str(api_humidity_value)
-        self.led_glow.show_message(y, 0.05, text_colour, back_colour)
+        y = float(api_humidity_value)
+        self.led_glow.show_message(str(y), 0.05, text_colour, back_colour)
     '''
     Default_LED colour (Pale White) when setBLUE or setRED function is not called
     '''
