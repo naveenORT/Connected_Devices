e4e82015f9b1c1f3a1d3fc154c8f420608571a67
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-02-21 03:07:55 -0500

    as5

--- apps/labs/module05/MultiActuatorAdaptor.py
+++ apps/labs/module05/MultiActuatorAdaptor.py
@@ -54,7 +54,7 @@ class MultiActuatorAdaptor():
         if (humidity_data_object.getActuationStae() is True):
                 self.api_actuator_status.addData("api_inbound", humidity_data_object.getcurvalue(), "Humidity_API")  # logging actuator data
                 obj = PersistenceUtil(self.api_actuator_status)
-                if(obj.get_Register_Actuator_Data() == True):
+                if(obj.get_Register_Actuator_Data().on_Actuator_Message() == True):
                     self.humi_api_message = SimpleLedActivator(10)            
                     logging.info("Going to Performing Actuation")         
                     self.humi_api_message.show_api_LED(humidity_data_object.getcurvalue())
