f4daf105150a16755bbce9b7f49ae9a63a563ae0
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-02-24 19:20:04 -0500

    latest

--- apps/tests/common/DataUtilTest.py
+++ apps/tests/common/DataUtilTest.py
@@ -6,18 +6,19 @@ from labs.common.AData import AData
 from labs.module05.TempSensorAdaptorTask import data_object
 import redis
 class DataUtilTest(unittest.TestCase):
-
+	redis_Server = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+	
 	def setUp(self):
 		self.msa = MultiSensorAdaptor()
 		self.msa.start()
 		self.sdm = SensorDataManager()
 		self.sdm.start()
-		self.redis_Server = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
-	def testjsontoactuatordata(self):
-		self.assertTrue(isinstance(util.jsonToActuatorData(self.redis_server.get(str(1))), AData),"Not an actuator_data")
+	
 		
 	def testsensordatatojson(self):
 		self.assertTrue(isinstance(util.sensordatatojson(data_object), str),"Not an actuator_data")
-
+	def testjsontoactuatordata(self):
+		self.assertTrue(isinstance(util.jsonToActuatorData(self.redis_server.get(str(1))), AData),"Not an actuator_data")
+	
 if __name__ == "__main__":
 	unittest.main()
