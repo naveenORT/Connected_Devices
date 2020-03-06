f96d07221674055252d07e61154b2810464bd77b
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-03-06 12:36:50 -0500

    Assign 6

--- apps/tests/common/DataUtilTest.py
+++ apps/tests/common/DataUtilTest.py
@@ -5,19 +5,34 @@ from labs.common.PersistenceUtil import util
 from labs.common.AData import AData
 from labs.module05.TempSensorAdaptorTask import data_object
 import redis
+'''
+* This class is for testing DataUtility tools which is used for converting from/to SensorData/ActuatorData to/from JSON string
+'''
 
 
 class DataUtilTest(unittest.TestCase):
-	
+
+	'''
+	* Starting up essential class functions & threads for the purpose of testing 
+	'''
+
 	def setUp(self):
 		self.msa = MultiSensorAdaptor()
 		self.msa.start()
 		self.sdm = SensorDataManager()
 		self.sdm.start()
 		self.redis_server = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+	
+	'''
+	* Function for testing whether sensordata is converted into JSON string 
+	'''
 		
 	def testsensordatatojson(self):
 		self.assertTrue(isinstance(util.sensordatatojson(data_object), str), "Not an actuator_data")
+
+	'''
+	* Function for testing whether json string is converted into actuatordata 
+	'''
 
 	def testjsontoactuatordata(self):
 		self.assertTrue(isinstance(util.jsonToActuatorData(self.redis_server.get(str(1))), AData), "Not an actuator_data")
--- apps/tests/labs/Module06Test.py
+++ apps/tests/labs/Module06Test.py
@@ -1,41 +1,40 @@
 import unittest
-
+from labs.module06.SensorDataManager import SensorDataManager
+from labs.module06.MultiSensorAdaptor import MultiSensorAdaptor    
+from labs.module06.TempSensorAdaptorTask import mqttc 
 
 """
-Test class for all requisite Module06 functionality.
-
-Instructions:
-1) Rename 'testSomething()' method such that 'Something' is specific to your needs; add others as needed, beginning each method with 'test...()'.
-2) Add the '@Test' annotation to each new 'test...()' method you add.
-3) Import the relevant modules and classes to support your tests.
-4) Run this class as unit test app.
-5) Include a screen shot of the report when you submit your assignment.
-
-Please note: While some example test cases may be provided, you must write your own for the class.
+* This class is for the purpose of testing public functions associated with module06
 """
+
+
 class Module06Test(unittest.TestCase):
 
 	"""
-	Use this to setup your tests. This is where you may want to load configuration
-	information (if needed), initialize class-scoped variables, create class-scoped
-	instances of complex objects, initialize any requisite connections, etc.
+	* Setting up essential threads & functions for testing	
 	"""
+
 	def setUp(self):
-		pass
+		msa = MultiSensorAdaptor()  # Thread 1
+		msa.start()  # Starting Thread
+		manager_object = SensorDataManager()  # Thread 2
+		manager_object.start()  # Starting
 
 	"""
-	Use this to tear down any allocated resources after your tests are complete. This
-	is where you may want to release connections, zero out any long-term data, etc.
+	* Function to check data published from mqttc
 	"""
-	def tearDown(self):
-		pass
+
+	def testPublishSensorData(self):
+		print(mqttc._on_publish.__sizeof__())
+		self.assertTrue(mqttc._on_publish.__sizeof__() > 0)
 
 	"""
-	Place your comments describing the test here.
+	* Function to check data published from mqttc
 	"""
-	def testSomething(self):
-		pass
+
+	def testConnect(self):
+		self.assertTrue(mqttc.is_connected() is True)
+
 
 if __name__ == "__main__":
-	#import sys;sys.argv = ['', 'Test.testName']
 	unittest.main()
