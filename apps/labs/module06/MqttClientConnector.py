34820581173839efe536fdedcf98d83a9eda4f84
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-03-14 13:14:05 -0400

    Assign 6

--- apps/labs/module06/MqttClientConnector.py
+++ apps/labs/module06/MqttClientConnector.py
@@ -30,28 +30,45 @@ class MqttClientConnector():
 
     '''
     * Publish SensorData object in json format to GatewayHandlerApp using MQTT 
+    * Input: mqtt-topic(String) & SensorData (Object)
     '''
         
     def publish_sensor_data(self, mqtt_topic, sensor_data):
         mqtt_message = json_tool.sensordatatojson(sensor_data)
         print(mqtt_message)
         mqttc.publish(mqtt_topic, mqtt_message, 2)
-    
+
+    '''
+    * public function for Subscribing to actuator_data
+    '''
+
     def subscribe_actuator_data(self):
         mqttc.subscribe("ActuatorData")
 
     def get_x(self):
         return self.publish_status
 
+'''
+* MQTT Callback function on connection establishment
+'''
+
 
 def on_connect():
         print("connection ok \n") 
 
-    
+'''
+* MQTT Callback function on publishing json data to MQTT Broker
+'''    
+
+
 def on_publish(mqttc, userdata, result):  # create function for callback
         publish_status = True
         print("Data Published \n")
         return publish_status
+'''
+* MQTT Callback function on receiving json ActuatorData via mqtt
+'''    
+
 
 def on_message(mqttc, userdata, message):
         print("message received " , str(message.payload.decode("utf-8")))
