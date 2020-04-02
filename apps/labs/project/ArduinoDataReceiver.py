075867ba1b55b2c861c548439b76e4bfe56337af
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-02 17:29:48 -0400

    project

--- apps/labs/project/ArduinoDataReceiver.py
+++ apps/labs/project/ArduinoDataReceiver.py
@@ -33,7 +33,7 @@ class ArduinoDataReceiver(threading.Thread):
     def run(self):
         while(1):
             self.receive_data_from_field()
-            time.sleep(3)
+            time.sleep(4)
     
     def receive_data_from_field(self):
         arduinoMessage = []
