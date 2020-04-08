29376de29209ec1c4f054ea31d7e252a3a3162b0
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-08 19:10:31 -0400

    asas

--- apps/labs/module09/DeviceHandlerApp.py
+++ apps/labs/module09/DeviceHandlerApp.py
@@ -8,9 +8,12 @@ from labs.module09.SensorHandlerApp import SensorHandlerApp
 import logging
 
 def main():
-
+    
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.info("Sensor_Logging_Started!!!")
     x = SensorHandlerApp()
     x.start()
-   
+    
     
 main()
