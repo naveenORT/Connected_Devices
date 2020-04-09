714bb65ccde7ca054dfe84101f68674fd6fe0dd7
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-09 03:52:49 -0400

    kj

--- apps/labs/module09/ArduinoDataReceiver.py
+++ apps/labs/module09/ArduinoDataReceiver.py
@@ -62,9 +62,9 @@ class ArduinoDataReceiver(threading.Thread):
             mag_y = round(mag["y"], 2)
             mag_z = round(mag["z"], 2)
             mag_t = sqrt(abs(mag_x * mag_x + mag_y * mag_y + mag_z * mag_z))
-            print("Magnetic Flux:" + str(mag_t))
+            print("Magnetic Flux:" + str(abs(mag_t)))
             # self.magnetic_flux = arduinoMessage[6] / 10
-            SensorData_Object.add_Mag_Value(mag_t)
+            SensorData_Object.add_Mag_Value(abs(mag_t))
             
         else:
             return
