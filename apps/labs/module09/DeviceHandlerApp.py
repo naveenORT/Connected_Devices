dcd77bb7870cec82931ec9d9d5e9475ede67f785
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-08 20:00:44 -0400

    asa

--- apps/labs/module09/DeviceHandlerApp.py
+++ apps/labs/module09/DeviceHandlerApp.py
@@ -7,7 +7,6 @@ Created on Apr 8, 2020
 from labs.module09.SensorHandlerApp import SensorHandlerApp
 import logging
 
-logging = logging()
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 def main():
