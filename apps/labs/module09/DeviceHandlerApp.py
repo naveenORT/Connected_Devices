48a442356c3a9aea275015d73c07e4013a497a63
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-09 22:59:54 -0400

    asa

--- apps/labs/module09/DeviceHandlerApp.py
+++ apps/labs/module09/DeviceHandlerApp.py
@@ -9,12 +9,14 @@ from labs.module09.DevicePerformanceMonitor import DevicePerformanceMonitor
 import time
 import logging
 
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 def main():
     
     
-    logging.getLogger("Main")
-    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger('Main')
+    logger.setLevel(logging.INFO)
+    
     
     SensorData_Object = ArduinoDataReceiver()
     SensorData_Object.start()
