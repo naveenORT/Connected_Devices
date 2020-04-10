74b28457a3d6207e7e52db06a80065155acb732c
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-09 23:42:29 -0400

    asas

--- apps/labs/module09/DeviceHandlerApp.py
+++ apps/labs/module09/DeviceHandlerApp.py
@@ -11,12 +11,14 @@ import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+logger.propagate = False
+
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
 ch.setFormatter(formatter)
+
 logger.addHandler(ch)
-logger.propagate = False
 
 
 
