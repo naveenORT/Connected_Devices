5483c6c0278057147a5220120660389293c11112
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-09 21:47:53 -0400

    asa

--- apps/labs/module09/DeviceHandlerApp.py
+++ apps/labs/module09/DeviceHandlerApp.py
@@ -12,8 +12,7 @@ import logging
 
 def main():
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
-    log = logging.getLogger().setLevel(logging.INFO)
-    log.info(".started logging.")
+    logging.getLogger().setLevel(logging.INFO)
 
     SensorData_Object = ArduinoDataReceiver()
     SensorData_Object.start()
