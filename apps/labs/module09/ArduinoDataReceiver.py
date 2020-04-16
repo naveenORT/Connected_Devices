1f7687c9a8c54286f1e95d79197cb621de15e400
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-16 12:55:02 -0400

    project

--- apps/labs/module09/ArduinoDataReceiver.py
+++ apps/labs/module09/ArduinoDataReceiver.py
@@ -15,14 +15,13 @@ pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xC2, 0xC2, 0xC2, 0xC2, 0xC2], [0x01, 
 SensorData_Object = SensorData()
 DeviceData_Object = DeviceData()
 sense = SenseHat()
-radio = NRF24(GPIO, spidev.SpiDev())
 
 
 class ArduinoDataReceiver(threading.Thread):
     
     def __init__(self):
         threading.Thread.__init__(self)
-        
+        radio = NRF24(GPIO, spidev.SpiDev())
         radio.begin(0, 17)        
         radio.setPayloadSize(32)
         radio.setChannel(0x76)
@@ -39,7 +38,7 @@ class ArduinoDataReceiver(threading.Thread):
         radio.startListening()
         
     def run(self):
-        radio.flush_rx()
+        self.radio.flush_rx()
         while(1):
             self.receive_data_from_cabindevice()
             self.receive_data_from_elecrticpit()
@@ -48,7 +47,7 @@ class ArduinoDataReceiver(threading.Thread):
     
     def receive_data_from_cabindevice(self):
         arduinoMessage = []
-        radio.read(arduinoMessage, self.radio.getDynamicPayloadSize())
+        self.radio.read(arduinoMessage, self.radio.getDynamicPayloadSize())
         
         if(arduinoMessage[0] == 1):
             # print("Received from Cabin Device: {}".format(arduinoMessage))
