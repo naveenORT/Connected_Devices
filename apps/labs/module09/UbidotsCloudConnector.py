f6bc920f03cbf529b979cf3d9bfbc89a46740915
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-16 18:08:20 -0400

    project

--- apps/labs/module09/UbidotsCloudConnector.py
+++ apps/labs/module09/UbidotsCloudConnector.py
@@ -52,7 +52,7 @@ class UbidotsCloudConnector(threading.Thread):
            # self.publish(mqtt_client, topic, sensor_payload)
             #self.publish(mqtt_client, topic, device_payload)
         
-            mqtt_client.subscribe("/v1.6/devices/substation-gateway/relay")
+            mqtt_client.subscribe("/v1.6/devices/substation-gateway/resistence")
             mqtt_client.on_message = on_message
             time.sleep(5)
         
