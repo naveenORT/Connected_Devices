2a31411a5a4c8a37ccff96b36c3b0d90fd6d8931
naveenORT <naveenORT@bitbucket.org/naveenORT/iot-device.git>
2020-04-24 23:57:52 -0400

    project

--- apps/labs/module09/GatewayHandlerApp.py
+++ apps/labs/module09/GatewayHandlerApp.py
@@ -3,6 +3,8 @@ Created on Apr 8, 2020
 @author: Naveen Rajendran
 '''
 import logging
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
 from labs.module09.ArduinoDataReceiver import ArduinoDataReceiver
 from labs.module09.OPC_Client_Rpi import OPC_Client_Rpi
 from labs.module09.SensorDataManager import SensorDataManager
@@ -17,7 +19,6 @@ performs particular dedicated function
 """    
 class GatewayHandlerApp():       
 
-    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
     
     SensorData_Object = ArduinoDataReceiver()  # Get Data from Constrained Device
     SensorData_Object.start()
